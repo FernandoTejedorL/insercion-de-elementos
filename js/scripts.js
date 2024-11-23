509b6ca6e4164266c0a0a278de6fc816b9b0b343
FernandoTejedorL <fernandotejedorlopez@gmail.com>
2024-11-23 13:44:24 +0100

    update

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
 const rootStyles = document.documentElement.style;
+const containerElement = document.getElementById('container');
+
+const newParagraphElement = document.createElement('p');
+
+newParagraphElement.textContent = 'soy un nuevo parrafo insertado';
+containerElement.append(newParagraphElement);
+containerElement.prepend(newParagraphElement);
+
+//append lo pone al final como hijo siguiente y prepend delante como hijo anterior
